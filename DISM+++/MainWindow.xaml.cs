0ed94a9da2ff351fc7832c04b77faf0a64aa5ff5
DrEthan <ethan.r.draper@gmail.com>
2021-05-10 14:58:11 -0600

    Initial Commit

---
+++ DISM+++/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using Ookii.Dialogs.Wpf;
+
+namespace DISM___
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "dism.exe";
+//            proc.StartInfo.Arguments = "/Online /Disable-Feature:Microsoft-Hyper-V-All";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.StartInfo.Verb = "runas";
+            proc.Start();
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void FindImageFrom(object sender, RoutedEventArgs e)
+        {
+            VistaFolderBrowserDialog openFileDialog = new VistaFolderBrowserDialog();
+            if (openFileDialog.ShowDialog() == true)
+                ImageFrom.Text = openFileDialog.SelectedPath;
+        }
+        
+        private void FindSaveTo(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "WIM Files | *.wim";
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = ".wim";
+            saveFileDialog.FileName = "Image";
+            if (saveFileDialog.ShowDialog() == true)
+               ImageTo.Text = saveFileDialog.FileName;
+        }
+        private void CreateImage(object sender, RoutedEventArgs e)
+        {
+            CanvasLabel.Text = "Create Image";
+            Create_Image_Canvas.Visibility = Visibility.Visible;
+            Apply_Image_Canvas.Visibility = Visibility.Hidden;
+            Diskpart_Canvas.Visibility = Visibility.Hidden;
+        }
+
+        private void ApplyImage(object sender, RoutedEventArgs e)
+        {
+            CanvasLabel.Text = "Apply Image";
+            Create_Image_Canvas.Visibility = Visibility.Hidden;
+            Apply_Image_Canvas.Visibility = Visibility.Visible;
+            Diskpart_Canvas.Visibility = Visibility.Hidden;
+        }
+
+        private void DiskPart(object sender, RoutedEventArgs e)
+        {
+            CanvasLabel.Text = "Disk Part";
+            Create_Image_Canvas.Visibility = Visibility.Hidden;
+            Apply_Image_Canvas.Visibility = Visibility.Hidden;
+            Diskpart_Canvas.Visibility = Visibility.Visible;
+        }
+    }
+}
